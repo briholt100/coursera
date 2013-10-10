6992bf490690a20a15695848fcc4279b4085cad6
briholt100 <brian.c.holt@gmail.com>
2013-10-09 22:48:32 -0700

    coding rankall.r

--- rankall.R
+++ rankall.R
@@ -6,25 +6,42 @@ directoryBase <- "C:/Documents and Settings/brian/My Documents/My Data Sources/M
 setwd(directoryBase)
 hospitals<-read.csv(file=paste0(directoryBase,"hospital-data.csv"),header=T)
 outcomes<-read.csv(file=paste0(directoryBase,"outcome-of-care-measures.csv"),colClasses="character")
+outcomes[,11]<-suppressWarnings(as.numeric(outcomes[,11]))
+outcomes[,17]<-suppressWarnings(as.numeric(outcomes[,17]))
+outcomes[,23]<-suppressWarnings(as.numeric(outcomes[,23]))
 
-rankall<-function(outcome){
+rankall<-function(outcome,num){
   
   outcome.list<- c("heart attack","heart failure","pneumonia")
-  #state.list<-levels(factor(outcomes$State))
-  #Initialize data frame
-  #for each state in outcomes make a little database?
+  state.list<-levels(factor(outcomes$State))
+  out.df<-data.frame()
+  
   
   #validate
-  
-  
-  if (outcome %in% outcome.list){print("yay!")}
+    
+  if (outcome %in% outcome.list){  }
   else {
     if(outcome %in% outcome.list==F){
       stop("invalid outcome")
+      } 
     }
+  
+  if (is.integer(num)|is.numeric(num)){print("yay")
+    #if(num > nrow(state.df)){return(NA)}
+    #else{
+     # return(as.character(state.df[num,5]))
+    #}
+    #for check    print(head(state.df,num+5))
   }
+  
+  for(state in state.list) {print(state)}
+    
+  state.df<-data.frame(outcomes[outcomes$State==state,c(7,11,17,23,2)]) #this adds state, attack, failure, pneu, name to dataframe
+  state.df<-cbind(state.df,as.integer(1))
+  colnames(state.df)<-c("state","heart attack","heart failure","pneumonia","name","Rank")  
 }
-#rankall("heart attack")
+#
+rankall("heart attack",5)
 
 
 
@@ -33,23 +50,13 @@ rankall<-function(outcome){
 
 
 
-#state.df<-data.frame(outcomes[outcomes$State==state,c(7,11,17,23,2)])
-#state.df<-cbind(state.df,as.integer(1))
-#colnames(state.df)<-c("state","heart attack","heart failure","pneumonia","name","Rank")
 
-#state.df[,2]<-suppressWarnings(as.numeric(state.df[,2]))
-#state.df[,3]<-suppressWarnings(as.numeric(state.df[,3]))
-#state.df[,4]<-suppressWarnings(as.numeric(state.df[,4]))
+
+
+
 #state.df<-state.df[order(state.df[,outcome],state.df[,"name"],na.last=T),]
 #state.df[,6]<-1:nrow(state.df)
 
-#if (is.integer(num)|is.numeric(num)){
- # if(num > nrow(state.df)){return(NA)}
-  #else{
-   # return(as.character(state.df[num,5]))
-  #}
-  #for check    print(head(state.df,num+5))
-#}
 
 
 #if(num=="best"){
