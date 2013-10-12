e40af657d7f4546357b6091f5d89280897438b12
briholt100 <brian.c.holt@gmail.com>
2013-10-12 11:00:11 -0700

    final rankall.r

--- rankall.R
+++ rankall.R
@@ -15,43 +15,31 @@ rankall<-function(outcome,num){
   outcome.list<- c("heart attack","heart failure","pneumonia")
   state.list<-levels(factor(outcomes$State))
   out.df<-data.frame()
-  
-  #validate
-    
   if (!outcome %in% outcome.list){stop("invalid outcome")  }
-    
-  #pull ranked outcomes and put into data.frame
       
   for(state in state.list) {
+    worst.index<-c()
+    worst.df<-data.frame()
     state.df<-data.frame(outcomes[outcomes$State==state,c(7,11,17,23,2)]) #this adds 
                 #state, heart attack, ht failure, pneu, name to dataframe
     state.df<-cbind(state.df,as.integer(1)) #this adds a 6th column for rank
     colnames(state.df)<-c("state","heart attack","heart failure","pneumonia","name","Rank") 
     state.df<-state.df[order(state.df[,outcome],state.df[,"name"],na.last=T),]
-    state.df[,6]<-1:nrow(state.df) #this adds the actual ranking
+    state.df[,6]<-1:nrow(state.df) #this adds the actual ranking    
     
-    
-    worst.index<-c()
-    worst.df<-data.frame()
-    
-    if(num=="best"){
-      num<-1
-    }
+    if(num=="best"){num<-1}
     
     if (is.integer(num)|is.numeric(num)){
         out.df<-rbind(out.df,state.df[num,c(5,1)])
-        colnames(out.df)<-c("hospital",state)}
+    }
 
-
-    if(num=="worst")
-{ 
+    if(num=="worst"){ 
        worst.index<-which(state.df[,outcome]>=max(state.df[,outcome],na.rm=T))
        out.df<-rbind(out.df,state.df[worst.index,c(5,1)])
-        colnames(out.df)<-c("hospital",state)}  
-    
+
+    }  
     }
+  colnames(out.df)<-c("hospital","state")
   print(out.df)
-  }
-
-
-rankall("heart attack",1)
+}
+#rankall("pneumonia","worst")
